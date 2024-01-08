0cf9854f9bc90342fa145ed8fd2c3bacf2724b24
Yusuf Solmaz <117117382+yusuf-s1@users.noreply.github.com>
2024-01-09 01:33:07 +0300

    Add files via upload

---
+++ BubleSort.java
@@ -0,0 +1,34 @@
+package BubleSort;
+
+import java.util.Arrays;
+
+public class BubleSort {
+    public static void bubbleSort(int arr[]) {
+        int i, j, temp;
+        boolean swapped;
+        for (i = 0; i < arr.length - 1; i++) {
+            swapped = false;
+            for (j = 0; j < arr.length - i -1; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)(Math.random() * 100 + 1);
+        }
+        System.out.println(Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+        
+    }
+}
